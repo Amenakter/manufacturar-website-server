2c9e79a504281a3780cb533988bc5fa65aa3aa50
Amenakter <amenakter27@gmail.com>
2022-05-26 12:07:50 +0600

    Jwt token create

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnn4g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verifay user email
 function verifyJTW(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
